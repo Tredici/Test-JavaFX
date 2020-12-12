0b5dd76401ad9a757680474895ecbfc4234e3731
Tredici <marcolucio1999@gmail.com>
2020-12-12 18:28:29 +0100

    Cambio scena elementare

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,36 @@
 package sample;
 
 import javafx.event.ActionEvent;
+// per il "cambio scena"
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.scene.text.Text;
+import javafx.scene.Group;
+
+import java.io.IOException;
 
 public class Controller {
-    public void btnPress(ActionEvent e) {
+    public void btnPress(ActionEvent e) throws IOException {
         System.out.println("Boom!");
+
+        /** Copiato [e rielaborato] senza alcuna vegogna da
+         * https://stackoverflow.com/questions/37200845/how-to-switch-scenes-in-javafx
+         */
+        Scene scene = ((Node)e.getSource()).getScene();
+
+        /** Copiato da:
+         * https://www.tutorialspoint.com/javafx/javafx_text.htm
+         */
+        Text text = new Text();
+        //Setting the text to be added.
+        text.setText("Hello how are you");
+        //setting the position of the text
+        text.setX(50);
+        text.setY(50);
+        //Creating a Group object
+        Group root = new Group(text);
+        scene.setRoot(root);
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,24 +6,74 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/** Per info:
+ * https://docs.oracle.com/javase/8/javafx/api/javafx/application/Application.html  (doc ufficiale)
+ * https://www.tutorialspoint.com/javafx/javafx_application.htm                     (utile spiegazione)
+ * http://tutorials.jenkov.com/javafx/stage.html#:~:text=%20JavaFX%20Stage%20%201%20Creating%20a%20Stage.,set%20a%20JavaFX%20Scene%20object%20on...%20More%20
+ *  (tutorial generale, anche con una minima introduzione alla gestione degli eventi)
+ *
+ */
 public class Main extends Application {
+
+    private int width = 300, height = 275;
 
     /*** Funzione che inizializza il tutto
      *  viene chiamata all'avvio dell'applicazione
+     *
+     *  Opzionale, può non essere implementato
      */
     @Override
     public  void init() throws Exception {
         System.out.println("Inizializzando...");
     }
 
+    /** Unico metodo da implementare obbligatoriamente
+     */
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+
+        /** Esempi di gestione eventi vari
+         * vedere il tutorial sopra
+         *
+         * NON hanno a che fare con il "bottone minimizza"
+         */
+        primaryStage.setOnHidden((event) -> {
+            System.out.println("Finestra nascosta");
+        });
+        // con le lambda con 1 arg. le parentesi non sono obbligatorie intorno all'arg
+        primaryStage.setOnShown(event -> {
+            /**
+             * Invocato prima che la finestra sia resa visibile (si chiama show su essa)
+             */
+            System.out.println("Finestra mostrata");
+        });
+
+        // Mostra la scena principale
+        showDefaultScene(primaryStage);
     }
 
+    public void showDefaultScene(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        stage.setScene(new Scene(root, this.width, this.height));
+        stage.show();
+    }
+    public void showOtherScene(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample-2.fxml"));
+        stage.setScene(new Scene(root, this.width, this.height));
+        stage.show();
+    }
+
+
+    /** Invocato alla chiusura
+     * di default invocherebbe super.stop() e basta
+     */
+    @Override
+    public void stop() throws Exception {
+        System.out.println("Stopping... (freeing resources)");
+
+        super.stop();
+    }
 
     public static void main(String[] args) {
         launch(args);
